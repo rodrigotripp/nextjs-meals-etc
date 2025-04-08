2b8a5a8d9a4ba3278cc64e8e3d66c2a9964b1f12
rodrigo <rdrgtrpp@gmail.com>
2025-04-08 15:49:35 +0300

    message: improviong style

--- app/components/Meal.tsx
+++ app/components/Meal.tsx
@@ -5,37 +5,41 @@ import { Meal as MealType } from '../types';
 function Meal(props: MealType) {
   const { strMeal, strMealThumb, strCategory, strArea, idMeal, refetch } = props;
   return (
-    <div className="rounded-lg border p-4">
-      <Link href={`/meal/${idMeal}`}>
-        <h3 className="text-lg font-medium">{strMeal}</h3>
-        {strMealThumb && (
-          <Image
-            priority={true}
-            src={strMealThumb}
-            alt={strMeal}
-            className="my-2 h-48 w-48 rounded object-cover"
-            width={400}
-            height={400}
-          />
+    <div className="rounded-lg border p-4 flex flex-row justify-around">
+      <div className='flex flex-col justify-around'>
+        <Link href={`/meal/${idMeal}`}>
+          <h3 className="text-lg font-medium">{strMeal}</h3>
+          {strMealThumb && (
+            <Image
+              priority={true}
+              src={strMealThumb}
+              alt={strMeal}
+              className="my-2 h-48 w-48 rounded object-cover"
+              width={400}
+              height={400}
+            />
+          )}
+          <p>
+            <strong>Category:</strong> {strCategory}
+          </p>
+          <p>
+            <strong>Origin:</strong> {strArea}
+          </p>
+          <p>
+            <strong>ID:{idMeal}</strong>
+          </p>
+        </Link>
+        {refetch && (
+          <button
+            onClick={() => refetch()}
+            className="mt-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+          >
+            Get Another Random Meal
+          </button>
         )}
-        <p>
-          <strong>Category:</strong> {strCategory}
-        </p>
-        <p>
-          <strong>Origin:</strong> {strArea}
-        </p>
-        <p>
-          <strong>ID:{idMeal}</strong>
-        </p>
-      </Link>
-      {refetch && (
-        <button
-          onClick={() => refetch()}
-          className="mt-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
-        >
-          Get Another Random Meal
-        </button>
-      )}
+        
+      </div>
+      <div className='max-w-xl hidden lg:inline '><p>{props.strInstructions}</p></div>
     </div>
   );
 }
