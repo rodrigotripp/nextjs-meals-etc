5b8732c00043181db7b43ebb73318d994a759ebd
Rodrigo Tripp <rodrigo.trippaguilar@elisa.fi>
2025-07-10 11:15:15 +0300

    message: improving style for ingredientes

--- app/ui/components/IngredientList/index.tsx
+++ app/ui/components/IngredientList/index.tsx
@@ -10,7 +10,7 @@ export const IngredientList = (
       <ul className='w-96'>
         {ingredients.map((item, index) => {
           return item.ingredient ? (
-            <li key={`${item.ingredient}_${index}`} className={`grid grid-cols-2 py-1 ${index % 2 === 0 ? 'bg-slate-200' : 'bg-slate-50'}`}>
+            <li key={`${item.ingredient}_${index}`} className={`grid grid-cols-2 py-1 px-2 ${index % 2 === 0 ? 'bg-slate-200' : 'bg-slate-50'}`}>
               <span className="font-medium">{item.ingredient}</span>
               <span className="text-gray-600">{item.measure}</span>
             </li>
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   images: {
     domains: ['www.themealdb.com'], // Add this line
+    // remotePatterns: [new URL('www.themealdb.com')]
   },
 };
 
